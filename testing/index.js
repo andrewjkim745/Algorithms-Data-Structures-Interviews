e04c3757fbb8285be6702386f19ac2f17136a72a
Andrew Kim <andrewk745@gmail.com>
2022-01-24 21:15:21 -0500

    testing set object

--- testing/index.js
+++ testing/index.js
@@ -1,7 +1,18 @@
-let n = 5
+// let n = 5
 
 
 
-let array = new Array(n).fill(0).map(a => ++a )
+// let array = new Array(n).fill(0).map(a => ++a )
 
-console.log(array)
+// console.log(array)
+
+let array = [1,2,3,4,5,6]
+
+let set = new Set(array)
+let stringN = '1,345'
+let parsed = parseInt(stringN)
+set.add({a: 1, b: 6})
+console.log(set)
+let stringArray = stringN.split('')
+
+
