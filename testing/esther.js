1676382af88ab3e0fc078001d40ae34115d8025c
Andrew Kim <andrewk745@gmail.com>
2022-02-21 10:31:03 -0500

    contains duplicates easyleet

--- testing/esther.js
+++ testing/esther.js
@@ -1,18 +1,27 @@
 
 
-let x = [1,2,3,4,5,6]
-let pooop = {
-    color: 'brown',
-    consistency: 'sticky',
-    smell: 'stinky'
+// let x = [1,2,3,4,5,6]
+// let pooop = {
+//     color: 'brown',
+//     consistency: 'sticky',
+//     smell: 'stinky'
+// }
+// console.log (pooop)
+// let emptyArray = []
+// const newArr = x.map(number => number + 2)
+// console.log(newArr)
+
+
+// console.log(x)
+
+function sortBy(a, p) {
+    return a.sort((a, b) => {
+        if (a[p] < b[p]) return 1
+        if (a[p] < b[p]) return -1
+        return 0
+    })
 }
-console.log (pooop)
-let emptyArray = []
-x.forEach(number => {
-    
-    emptyArray.push(number + 2)
-})
-console.log(emptyArray)
 
 
 
+console.log(sortBy())
